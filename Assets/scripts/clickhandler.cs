970f5de104200387db73472e5d8bc0302afaa754
enemali <malitician@gmail.com>
2021-10-26 13:57:15 +0100

    Waiting room
    Waiting room scene first push ( watson exiting after about 30 sec.)

---
+++ Assets/scripts/clickhandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class clickhandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public unityEvent upEvent;
+    //public unityEvent downEvent;
+  void OnMouseDown() {
+      Debug.Log("down");
+    
+}
+  void OnMouseUp() {
+      Debug.Log("up");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
